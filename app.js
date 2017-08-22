f4843887740a72884e69402dcfd01027329270dc
lvl <marian.razvan.oprisan@gmail.com>
2017-08-22 15:33:37 +0300

    Added express-validator for form and improved UI

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const expressValidator = require('express-validator');
 var app = express();
 // logger is a custom middleware; must be declared before app.route() code
 // var logger = function(req, res, next) {
@@ -9,6 +10,12 @@ var app = express();
 //     next();
 // }
 // app.use(logger);
+
+//Global Vars
+app.use(function(req, res, next) {
+    res.locals.errors = null;
+    next();
+})
 
 // view engine
 app.set('view engine', 'ejs');
@@ -21,6 +28,25 @@ app.use(bodyParser.urlencoded({extended: false}));
 // If we have an index.html file in the public folder, the Hello World! would be replaced by its contents
 // angular apps are used this way, and they are usually found in the public folder
 //app.use(express.static(path.join(__dirname, 'public')));
+
+// In this snippet, the formParam value is going to get morphed into form body format useful for printing
+// imported from express-validator's documentation
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+        , root    = namespace.shift()
+        , formParam = root;
+  
+      while(namespace.length) {
+        formParam += '[' + namespace.shift() + ']';
+      }
+      return {
+        param : formParam,
+        msg   : msg,
+        value : value
+      };
+    }
+  }));
 
 var people = [{
         name: 'Jeff',
@@ -56,10 +82,43 @@ var users = [
 
 app.get('/', function(req, res) {
     res.render('index', {
-        title: 'EJS View Engine',
+        title: 'Express Crash Course',
         users: users
     });
+    //res.end();
 }); 
+
+app.post('/users/add', function(req, res) {
+    // from express-validator
+    req.checkBody('firstName', 'First Name is required').notEmpty();
+    req.checkBody('lastName', 'Last Name is required').notEmpty();
+    req.checkBody('email', 'Email is required').notEmpty();
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+        res.render('index', {
+            title: 'Express Crash Course',
+            users: users,
+            errors: errors
+        });
+        //res.end();
+    } else {
+        var newUser = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email
+        }
+        console.log('Success');
+        users.push(newUser);
+        res.render('index', {
+            title: 'Express Crash Course',
+            users: users,
+        });
+        //res.end();
+    }
+
+});
 
 app.listen(3000, function() {
     console.log('Server started on Port 3000');
--- readme.md
+++ readme.md
@@ -1,8 +1,17 @@
 ## Express Crash Course(traversyMedia)
 ---
-From the TraversyMedia YouTube channel
+From the [TraversyMedia](https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA) YouTube channel
 
 ---
 Notes:
+
+
     * Using nodemon to automate app restarting after making changes
+
+
     * Using ejs templating language(basically html with embedded variables)
+
+
+    * Using express-validator to validate user input
+
+    
